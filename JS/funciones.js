25bc2d6500c1c49d4c876984150a51d43df60379
Leslie <lesliemelendezmg@gmail.com>
2021-12-14 12:45:59 -0500

    G

---
+++ JS/funciones.js
@@ -0,0 +1,57 @@
+let aud1 = document.getElementById("aud1");
+let btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", () =>{
+  aud1.play();
+});
+
+let aud2 = document.getElementById("aud2");
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", () =>{
+  aud2.play();
+});
+
+let aud3 = document.getElementById("aud3");
+let btn3 = document.getElementById("btn3");
+btn3.addEventListener("click", () =>{
+  aud3.play();
+});
+
+let aud4 = document.getElementById("aud4");
+let btn4 = document.getElementById("btn4");
+btn4.addEventListener("click", () =>{
+  aud4.play();
+});
+
+let aud5 = document.getElementById("aud5");
+let btn5 = document.getElementById("btn5");
+btn5.addEventListener("click", () =>{
+  aud5.play();
+});
+
+let aud6 = document.getElementById("aud6");
+let btn6 = document.getElementById("btn6");
+btn6.addEventListener("click", () =>{
+  aud6.play();
+});
+let aud7 = document.getElementById("aud7");
+let btn7 = document.getElementById("btn7");
+btn7.addEventListener("click", () =>{
+  aud7.play();
+})
+let aud8 = document.getElementById("aud8");
+let btn8 = document.getElementById("btn8");
+btn8.addEventListener("click", () =>{
+  aud8.play();
+})
+let aud9 = document.getElementById("aud9");
+let btn9 = document.getElementById("btn9");
+btn9.addEventListener("click", () =>{
+  aud9.play();
+})
+
+
+
+
+
+
+
